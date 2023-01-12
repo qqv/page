6ecdc22cb3e19349483b57150b4b4729ad02b60b
unknown <vrad@qq.com>
2023-01-12 13:21:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,36 +5,36 @@
 
 int main() {
   // Initialize the HAL and Vehicle objects
-  DIABLO::OSDK::HAL hal;
+  auto hal = std::make_shared<DIABLO::OSDK::HAL>();
   DIABLO::OSDK::Vehicle vehicle(&hal);
 
   // Obtain control of the robot
-  if (vehicle.movement_ctrl.obtain_control()) {
+  if (vehicle->movement_ctrl->obtain_control()) {
     std::cerr << "Failed to obtain control" << std::endl;
     return -1;
   }
 
   // Set the movement control mode to Cartesian
-  if (vehicle.movement_ctrl.SendMovementModeCtrlCmd()) {
+  if (vehicle->movement_ctrl->SendMovementModeCtrlCmd()) {
     std::cerr << "Failed to set movement control mode" << std::endl;
     return -1;
   }
 
 // Set the movement command to move forward at 0.5 m/s for 5 seconds
-   vehicle.movement_ctrl.ctrl_data.vx = 0.5f;
-   vehicle.movement_ctrl.ctrl_data.vy = 0;
-   vehicle.movement_ctrl.ctrl_data.vz = 0;
-   vehicle.movement_ctrl.ctrl_data.yaw = 0;
-   vehicle.movement_ctrl.ctrl_data.duration = 5000; // 5 seconds in milliseconds
+   vehicle->movement_ctrl->ctrl_data.vx = 0.5f;
+   vehicle->movement_ctrl->ctrl_data.vy = 0;
+   vehicle->movement_ctrl->ctrl_data.vz = 0;
+   vehicle->movement_ctrl->ctrl_data.yaw = 0;
+   vehicle->movement_ctrl->ctrl_data.duration = 5000; // 5 seconds in milliseconds
 
   // Send the movement command
-  if (vehicle.movement_ctrl.SendMovementCtrl()) {
-    std::cerr << "Failed to send movement control command" << std::endl;
+  if (vehicle->movement_ctrl->SendMovementCtrl()) {
+  std::cerr << "Failed to send movement control command" << std::endl;
     return -1;
   }
 
   // Release control of the robot
-  if (vehicle.movement_ctrl.release_control()) {
+  if (vehicle->movement_ctrl->release_control()) {
     std::cerr << "Failed to release control" << std::endl;
     return -1;
   }
