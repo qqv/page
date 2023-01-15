b282d64dc69834dc41877807b4c8f39dface77c0
unknown <vrad@qq.com>
2023-01-15 21:29:50 +0800

    rewrite

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ def index():
     return render_template('index.html')
 
 @app.route('/teleop', methods=['POST'])
-def receive_teleop_command():
-    command = request.json['command']
-    teleop(command)
-    return 'Command received'
+def teleop():
+    command = request.form['command']
+    value = request.form['value']
+    teleop.run_command(command, value)
+    return 'Command sent'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
 
--- teleop_script/teleop.py
+++ teleop_script/teleop.py
@@ -15,70 +15,36 @@ CMD_HEIGH_MODE = 0x01
 CMD_BODY_UP = 0x11
 
 CMD_STAND_UP = 0x02
-CMD_STAND_DOWN  =  0x12
+CMD_STAND_DOWN = 0x12
 
 CMD_PITCH = 0x03
 CMD_PITCH_MODE = 0x13
 
 CMD_SPEED_MODE = 0x05
 
-def teleop(command):
-    rclpy.init() 
-    node = Node("diablo_teleop_node")  
+def run_command(command, value):
+    rclpy.init()
+    node = Node("diablo_teleop_node")
     msg = MotionCtrl()
     teleop_cmd = node.create_publisher(MotionCtrl,"diablo/MotionCmd",2)
-    if command == 'w':
+    if command == 'go_forward':
         msg.cmd_id = CMD_GO_FORWARD
-        msg.value = 1.0
-    elif command == 's':
-        msg.cmd_id = CMD_GO_FORWARD
-        msg.value = -(1.0)
-    elif command == 'a':
+        msg.value = value
+    elif command == 'go_left':
         msg.cmd_id = CMD_GO_LEFT
-        msg.value = 1.0
-    elif command == 'd':
-        msg.cmd_id = CMD_GO_LEFT
-        msg.value = -(1.0)
-
-    elif command == 'e':
+        msg.value = value
+    elif command == 'roll_right':
         msg.cmd_id = CMD_ROLL_RIGHT
-        msg.value = 0.1
-    elif command == 'q':
-        msg.cmd_id = CMD_ROLL_RIGHT
-        msg.value = -(0.1)
-    elif command == 'r':
-        msg.cmd_id = CMD_ROLL_RIGHT
-        msg.value = 0.0
-
-    elif command == 'h':
+        msg.value = value
+    elif command == 'body_up':
         msg.cmd_id = CMD_BODY_UP
-        msg.value = -(0.5)
-    elif command == 'j':
-        msg.cmd_id = CMD_BODY_UP
-        msg.value = 1.0
-    elif command == 'k':
-        msg.cmd_id = CMD_BODY_UP
-        msg.value = 0.5
-    elif command == 'l':
-        msg.cmd_id = CMD_BODY_UP
-        msg.value = 0.0
-    elif command == 'u':
+        msg.value = value
+    elif command == 'pitch':
         msg.cmd_id = CMD_PITCH
-        msg.value = 0.5
-    elif command == 'i':
-        msg.cmd_id = CMD_PITCH
-        msg.value = 0.0
-    elif command == 'o':
-        msg.cmd_id = CMD_PITCH
-        msg.value = -(0.5)
-    
+        msg.value = value
     else:
         msg.cmd_id = 0x00
         msg.value = 0.0
 
-    teleop_cmd.publish(msg)
-    rclpy.spin_once(node)
-    
-
-
-
+    teleop_cmd. publish(msg)
+    rclpy. spin_once(node)
